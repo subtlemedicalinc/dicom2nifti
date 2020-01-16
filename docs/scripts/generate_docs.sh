6722420a7673d36437e4358ce3cb2a7c77c91820
Arne Brys <arne.brys@icometrix.com>
2020-01-16 15:11:10 +0100

    icometrix/dicom2nifti#67: Update script

--- docs/scripts/generate_docs.sh
+++ docs/scripts/generate_docs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 # update the api docs
 sphinx-apidoc -f -o ../rst ../../dicom2nifti
-rm -Rf ../html
-sphinx-build -b html ../rst ../html
-touch ../html/.nojekyll
+rm -Rf ../../../dicom2nifti-docs/*
+sphinx-build -b html ../rst ../../../dicom2nifti-docs/
+touch ../../../dicom2nifti-docs/.nojekyll
